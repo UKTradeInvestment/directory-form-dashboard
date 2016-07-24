83c6a6d8677fcab320d4b156ebdc10e0772c17e2
Zac Tolley <zac@thetolleys.com>
2016-07-24 13:19:57 +0100

    Add config. Requires separate secrets file with key details and environment var with view.

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 'use strict';
 
+var secrets = require('./secrets.json');
+
 const port = process.env.PORT || 3000;
 
-module.exports = {
+	module.exports = {
   env: process.env.NODE_ENV,
   port: port,
   ga: {
-    client_email: process.env.GA_CLIENT_EMAIL,
-    private_key: process.env.GA_PRIVATE_KEY,
-    view_id: process.env.GA_VIEW_ID
+    clientEmail: secrets.client_email,
+    privateKey: secrets.private_key,
+		viewID: process.env.GA_VIEW_ID
   }
 };
